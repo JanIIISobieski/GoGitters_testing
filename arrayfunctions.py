1f4499f1ca223f4832690af31775fef14197b06b
Stephanie Hatz <seh46@duke.edu>
2018-02-09 13:10:09 -0500

    Changed tuples to pep8 formatting

--- arrayfunctions.py
+++ arrayfunctions.py
@@ -4,5 +4,5 @@ import numpy as np
 def min_max(number_list):
     min_val = np.amin(number_list)
     max_val = np.amax(number_list)
-    minmax_out = (min_val,max_val)
+    minmax_out = (min_val, max_val)
     return minmax_out
